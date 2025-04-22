15cc99be2c3bb70ca8a4ce7e2f70417bbceeeb51
imanol <imanolrtega@gmail.com>
2025-04-22 00:14:11 -0300

    Fix Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/once-ui/styles/index.scss'
 import '@/once-ui/tokens/index.scss'
+import { Analytics } from '@vercel/analytics/react'
 
 import classNames from 'classnames'
 
@@ -75,6 +76,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
       data-transition={style.transition}
       className={classNames(primary.variable, GeistSans.variable)}
     >
+      <Analytics />
       <ToastProvider>
         <Column
           style={{ minHeight: '100vh' }}
