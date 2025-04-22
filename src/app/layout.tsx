783d248c8bdec8b7d68d62b717671b5361cbc45e
imanol <imanolrtega@gmail.com>
2025-04-22 00:39:01 -0300

    Add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/once-ui/styles/index.scss'
 import '@/once-ui/tokens/index.scss'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import classNames from 'classnames'
 
@@ -77,6 +78,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
       className={classNames(primary.variable, GeistSans.variable)}
     >
       <Analytics />
+      <SpeedInsights />
       <ToastProvider>
         <Column
           style={{ minHeight: '100vh' }}
