9f8d96866b9390e022426b3b719bc39793f08389
imanol <imanolrtega@gmail.com>
2025-04-23 16:10:29 -0300

    Add G Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/once-ui/styles/index.scss'
 import '@/once-ui/tokens/index.scss'
 import { Analytics } from '@vercel/analytics/react'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import classNames from 'classnames'
@@ -79,6 +80,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
     >
       <Analytics />
       <SpeedInsights />
+      <GoogleAnalytics gaId="G-SZLNLGY5NV" />
       <ToastProvider>
         <Column
           style={{ minHeight: '100vh' }}
